8220b554fec506a42c967ce39b951230a135eb62
Dan1elShastovsckiy <44569206+Dan1elShastovsckiy@users.noreply.github.com>
2025-09-23 21:37:37 +0300

    Реализация темной темы

--- lib/utils/max_width_extension.dart
+++ lib/utils/max_width_extension.dart
@@ -1,28 +1,61 @@
+// lib/utils/max_width_extension.dart
+
 import 'package:flutter/material.dart';
 import 'package:responsive_framework/responsive_framework.dart';
 
+// Мы создадим виджет MaxWidthBox отдельно, чтобы избежать дублирования
+class MaxWidthBox extends StatelessWidget {
+  final double maxWidth;
+  final Widget child;
+  final EdgeInsets padding;
+  // Убираем backgroundColor, он больше не нужен
+  
+  const MaxWidthBox({
+    super.key, 
+    required this.maxWidth, 
+    required this.child, 
+    this.padding = const EdgeInsets.symmetric(horizontal: 32)
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        padding: padding,
+        constraints: BoxConstraints(maxWidth: maxWidth),
+        child: child,
+      ),
+    );
+  }
+}
+
+
 extension MaxWidthExtension on List<Widget> {
   List<Widget> toMaxWidth() {
-    return map(
-      (item) => MaxWidthBox(
+    return [
+      MaxWidthBox(
         maxWidth: 1200,
-        padding: const EdgeInsets.symmetric(horizontal: 32),
-        backgroundColor: Colors.white,
-        child: item,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: this,
+        ),
       ),
-    ).toList();
+    ];
   }
 
   List<Widget> toMaxWidthSliver() {
-    return map(
-      (item) => SliverToBoxAdapter(
+    return [
+      SliverToBoxAdapter(
         child: MaxWidthBox(
           maxWidth: 1200,
-          padding: const EdgeInsets.symmetric(horizontal: 32),
-          backgroundColor: Colors.white,
-          child: item,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: this,
+          ),
         ),
       ),
-    ).toList();
+    ];
   }
 }
