7238a4987de08cea0e2babc8890c6c4ef7f9b31a
Dan1elShastovsckiy <44569206+Dan1elShastovsckiy@users.noreply.github.com>
2025-09-23 21:39:54 +0300

    Реализация темной темы

--- lib/lib/main.dart
+++ lib/lib/main.dart
@@ -2,10 +2,11 @@
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-// ignore: depend_on_referenced_packages
 import 'package:flutter_web_plugins/url_strategy.dart';
 import 'package:minimal/components/route_generator.dart';
-import 'package:minimal/utils/no_animation_page_transitions_builder.dart';
+import 'package:minimal/components/theme.dart';
+import 'package:minimal/components/theme_provider.dart';
+import 'package:provider/provider.dart';
 import 'package:responsive_framework/responsive_framework.dart';
 
 void main() {
@@ -23,36 +24,29 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      // обертка ResponsiveBreakpoints
-      builder: (context, child) => ResponsiveBreakpoints.builder(
-        breakpoints: [
-          const Breakpoint(start: 0, end: 450, name: MOBILE),
-          const Breakpoint(start: 451, end: 800, name: TABLET),
-          const Breakpoint(start: 801, end: 1920, name: DESKTOP),
-          const Breakpoint(start: 1921, end: double.infinity, name: '4K'),
-        ],
-        child: child!,
+    return ChangeNotifierProvider(
+      create: (_) => ThemeProvider(),
+      child: Consumer<ThemeProvider>(
+        builder: (context, themeProvider, child) {
+          return MaterialApp(
+            builder: (context, child) => ResponsiveBreakpoints.builder(
+              breakpoints: [
+                const Breakpoint(start: 0, end: 450, name: MOBILE),
+                const Breakpoint(start: 451, end: 800, name: TABLET),
+                const Breakpoint(start: 801, end: 1920, name: DESKTOP),
+                const Breakpoint(start: 1921, end: double.infinity, name: '4K'),
+              ],
+              child: child!,
+            ),
+            theme: AppTheme.lightTheme,
+            darkTheme: AppTheme.darkTheme,
+            themeMode: themeProvider.themeMode,
+            debugShowCheckedModeBanner: false,
+            initialRoute: '/',
+            onGenerateRoute: RouteGenerator.generateRoute,
+          );
+        },
       ),
-
-      // Настройки темы
-      theme: ThemeData(
-        pageTransitionsTheme: PageTransitionsTheme(
-          builders: {
-            TargetPlatform.fuchsia: NoAnimationPageTransitionsBuilder(),
-            TargetPlatform.linux: NoAnimationPageTransitionsBuilder(),
-            TargetPlatform.macOS: NoAnimationPageTransitionsBuilder(),
-            TargetPlatform.windows: NoAnimationPageTransitionsBuilder(),
-          },
-        ),
-      ),
-      debugShowCheckedModeBanner: false,
-
-      // --- ЛОГИКА МАРШРУТИЗАЦИИ ---
-      initialRoute: '/',
-      onGenerateRoute: RouteGenerator.generateRoute,
-      // onUnknownRoute можно не указывать, так как onGenerateRoute
-      // уже обрабатывает все случаи через default в switch.
     );
   }
 }
