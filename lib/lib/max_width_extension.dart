f3fadc232d34782fdb8669c12077e013e0e91f20
Dan1elShastovsckiy <44569206+Dan1elShastovsckiy@users.noreply.github.com>
2025-09-25 13:17:27 +0300

    Новая таблица

---
+++ lib/lib/max_width_extension.dart
@@ -0,0 +1,61 @@
+// lib/utils/max_width_extension.dart
+
+import 'package:flutter/material.dart';
+import 'package:responsive_framework/responsive_framework.dart';
+
+// Мы создадим виджет MaxWidthBox отдельно, чтобы избежать дублирования
+class MaxWidthBox extends StatelessWidget {
+  final double maxWidth;
+  final Widget child;
+  final EdgeInsets padding;
+  // Убираем backgroundColor, он больше не нужен
+  
+  const MaxWidthBox({
+    super.key, 
+    required this.maxWidth, 
+    required this.child, 
+    this.padding = const EdgeInsets.symmetric(horizontal: 32)
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        padding: padding,
+        constraints: BoxConstraints(maxWidth: maxWidth),
+        child: child,
+      ),
+    );
+  }
+}
+
+
+extension MaxWidthExtension on List<Widget> {
+  List<Widget> toMaxWidth() {
+    return [
+      MaxWidthBox(
+        maxWidth: 1200,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: this,
+        ),
+      ),
+    ];
+  }
+
+  List<Widget> toMaxWidthSliver() {
+    return [
+      SliverToBoxAdapter(
+        child: MaxWidthBox(
+          maxWidth: 1200,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: this,
+          ),
+        ),
+      ),
+    ];
+  }
+}
